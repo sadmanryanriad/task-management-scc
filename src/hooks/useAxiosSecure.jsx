adbec84ef29c0ab5986b51f9075cb71c6d8f5042
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-22 18:28:22 +0600

    deployed to firebase

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "https://collaborative-task-management-server-blond.vercel.app/"
 })
 
 const useAxiosSecure = () => {
