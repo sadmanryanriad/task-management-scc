9c0d9a5b67912bc84022476ca2239810ce7fa192
sadmanryanriad <sadmanryanriad@gmail.com>
2023-12-22 16:34:14 +0600

    store task to database

--- src/components/Pages/dashboard/Dashboard.jsx
+++ src/components/Pages/dashboard/Dashboard.jsx
@@ -1,9 +1,32 @@
+import { useContext } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Header from "../../miniComponents/Header";
 import TaskInputForm from "./TaskInputForm";
+import { AuthContext } from "../../../provider/AuthProvider";
+import toast from "react-hot-toast";
 
 const Dashboard = () => {
+    const {user} = useContext(AuthContext);
+    const axiosSecure = useAxiosSecure();
   const handleTaskSubmit = (data) => {
     console.log("Submitted data:", data);
+    const taskInfo = {
+        email: user?.email,
+        title: data.title,
+        description: data.description,
+        priority: data.priority,
+        dueDate: data.dueDate,
+        status: "todo",
+    }
+    axiosSecure.post("/tasks", taskInfo)
+    .then(res=>{
+        // console.log(res.data);
+        if(res.data.insertedId){
+            toast.success("Task added successfully");
+        }else{
+            toast.error("Something went wrong!");
+        }
+    })
   };
 
   return (
